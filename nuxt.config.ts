a107b483c83498c6f7aa51de83ae8200da8fb5cc
isiah <jones.isiah.blhs@gmail.com>
2024-04-17 20:56:06 -0400

    add remix icons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: false,
   devtools: { enabled: true },
   css: ["~/assets/scss/styles.scss"],
+  app: {
+    head: {
+      link: [
+        {
+          rel: "stylesheet",
+          href: "https://cdnjs.cloudflare.com/ajax/libs/remixicon/4.2.0/remixicon.min.css",
+        },
+      ],
+    },
+  },
 });
