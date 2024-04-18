a45179c330a44721a588aee05d47094795bfee59
isiah <jones.isiah.blhs@gmail.com>
2024-04-17 21:05:57 -0400

    add the javascript to show nav

--- components/header.vue
+++ components/header.vue
@@ -1,4 +1,53 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+onMounted(() => {
+  /*=============== SHOW MENU ===============*/
+  const navMenu = document.getElementById("nav-menu"),
+    navToggle = document.getElementById("nav-toggle"),
+    navClose = document.getElementById("nav-close");
+
+  /* Menu show */
+  if (navToggle) {
+    navToggle.addEventListener("click", () => {
+      if (navMenu) {
+        navMenu.classList.add("show-menu");
+      }
+    });
+  }
+
+  /* Menu hidden */
+  if (navClose) {
+    navClose.addEventListener("click", () => {
+      if (navMenu) {
+        navMenu.classList.remove("show-menu");
+      }
+    });
+  }
+
+  /*=============== REMOVE MENU MOBILE ===============*/
+  const navLink = document.querySelectorAll(".nav__link");
+
+  const linkAction = () => {
+    const navMenu = document.getElementById("nav-menu");
+    // When we click on each nav__link, we remove the show-menu class
+    if (navMenu) {
+      navMenu.classList.remove("show-menu");
+    }
+  };
+  navLink.forEach((n) => n.addEventListener("click", linkAction));
+
+  /*=============== SHADOW HEADER ===============*/
+  const shadowHeader = () => {
+    const header = document.getElementById("header");
+    // Add a class if the bottom offset is greater than 50 of the viewport
+    if (header) {
+      window.scrollY >= 50
+        ? header.classList.add("shadow-header")
+        : header.classList.remove("shadow-header");
+    }
+  };
+  window.addEventListener("scroll", shadowHeader);
+});
+</script>
 
 <template>
   <header class="header" id="header">
