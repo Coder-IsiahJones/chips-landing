600d13dd7fcb2e5a40c2a410f08299d21b415ca7
isiah <jones.isiah.blhs@gmail.com>
2024-04-17 21:37:47 -0400

    improve code with v-for

--- components/favorite.vue
+++ components/favorite.vue
@@ -1,4 +1,27 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import cheeseImage from "@/assets/img/cheese.png";
+import favoriteChips1Image from "@/assets/img/favorite-chips-1.png";
+import favoriteChips2Image from "@/assets/img/favorite-chips-2.png";
+import favoriteChips3Image from "@/assets/img/favorite-chips-3.png";
+import leaf1Image from "@/assets/img/leaf-1.png";
+import leaf2Image from "@/assets/img/leaf-2.png";
+
+// Define an array of image sources for cheese images
+const cheeseImages = [
+  { src: cheeseImage, alt: "Cheese" },
+  { src: cheeseImage, alt: "Cheese" },
+];
+
+// Define an array of image sources for favorite chips
+const favoriteChips = [
+  favoriteChips1Image,
+  favoriteChips2Image,
+  favoriteChips3Image,
+];
+
+// Define image sources for leaf images
+const leafImages = [leaf1Image, leaf2Image];
+</script>
 
 <template>
   <section class="favorite section" id="favorites">
@@ -14,74 +37,33 @@
           delicious snack recipes from around the world for you.
         </p>
 
+        <!-- Loop through cheese images -->
         <img
-          src="assets/img/cheese.png"
-          alt="image"
-          class="favorites__cheese-1"
-        />
-        <img
-          src="assets/img/cheese.png"
-          alt="image"
-          class="favorites__cheese-2"
+          v-for="(cheese, index) in cheeseImages"
+          :key="index"
+          :src="cheese.src"
+          :alt="cheese.alt"
+          :class="`favorites__cheese-${index + 1}`"
         />
       </div>
 
       <div class="favorites__swiper swiper">
         <div class="swiper-wrapper">
-          <article class="favorites__article swiper-slide">
-            <img
-              src="assets/img/favorite-chips-1.png"
-              alt="image"
-              class="favorites__img"
-            />
+          <!-- Loop through favorite chips -->
+          <article
+            v-for="(chip, index) in favoriteChips"
+            :key="index"
+            class="favorites__article swiper-slide"
+          >
+            <img :src="chip" alt="Favorite Chips" class="favorites__img" />
 
+            <!-- Loop through leaf images -->
             <img
-              src="assets/img/leaf-1.png"
-              alt="image"
-              class="favorites__leaf-1"
-            />
-            <img
-              src="assets/img/leaf-2.png"
-              alt="image"
-              class="favorites__leaf-2"
-            />
-          </article>
-
-          <article class="favorites__article swiper-slide">
-            <img
-              src="assets/img/favorite-chips-2.png"
-              alt="image"
-              class="favorites__img"
-            />
-
-            <img
-              src="assets/img/leaf-1.png"
-              alt="image"
-              class="favorites__leaf-1"
-            />
-            <img
-              src="assets/img/leaf-2.png"
-              alt="image"
-              class="favorites__leaf-2"
-            />
-          </article>
-
-          <article class="favorites__article swiper-slide">
-            <img
-              src="assets/img/favorite-chips-3.png"
-              alt="image"
-              class="favorites__img"
-            />
-
-            <img
-              src="assets/img/leaf-1.png"
-              alt="image"
-              class="favorites__leaf-1"
-            />
-            <img
-              src="assets/img/leaf-2.png"
-              alt="image"
-              class="favorites__leaf-2"
+              v-for="(leaf, leafIndex) in leafImages"
+              :key="leafIndex"
+              :src="leaf"
+              alt="Leaf"
+              :class="`favorites__leaf-${leafIndex + 1}`"
             />
           </article>
         </div>
@@ -89,5 +71,3 @@
     </div>
   </section>
 </template>
-
-<style></style>
