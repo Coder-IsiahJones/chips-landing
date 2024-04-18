ba426c603aaf7750bcdda8af9e97fd522b38f0a3
isiah <jones.isiah.blhs@gmail.com>
2024-04-17 20:57:38 -0400

    add components to app.cuv

--- app.vue
+++ app.vue
@@ -1,5 +1,19 @@
+<script lang="ts" setup></script>
+
 <template>
   <div>
-    <NuxtWelcome />
+    <Header class="header" />
+
+    <main class="main">
+      <Home />
+      <Favorite />
+      <Care />
+      <Banner />
+      <Product />
+      <Contact />
+    </main>
+
+    <LazyFooter />
+    <LazyScrollUp />
   </div>
 </template>
