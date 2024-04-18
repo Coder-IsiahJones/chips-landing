7b9efe0d028f063703393bf279fcbbc20acfccc9
isiah <jones.isiah.blhs@gmail.com>
2024-04-17 20:54:30 -0400

    create footer component

---
+++ components/footer.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup></script>
+
+<template>
+  <footer class="footer">
+    <div class="footer__container container grid">
+      <a href="#" class="footer__logo">CHIPS</a>
+
+      <div class="footer__content grid">
+        <a href="#" class="footer__link">Terms & Agreements</a>
+        <a href="#" class="footer__link">Privacy Policy</a>
+
+        <span class="footer__copy">
+          &#169; All Rights Reserved By Front-End Challenge
+        </span>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<style></style>
