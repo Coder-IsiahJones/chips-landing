76f817a7b20176afaccdff25ade55cc85c98545a
isiah <jones.isiah.blhs@gmail.com>
2024-04-17 21:10:30 -0400

    add the scroll up component

---
+++ components/scroll-up.vue
@@ -0,0 +1,48 @@
+<script lang="ts" setup>
+onMounted(() => {
+  const scrollUp = () => {
+    const scrollUp = document.getElementById("scroll-up");
+    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+
+    if (scrollUp) {
+      window.scrollY >= 350
+        ? scrollUp.classList.add("show-scroll")
+        : scrollUp.classList.remove("show-scroll");
+    }
+  };
+  window.addEventListener("scroll", scrollUp);
+
+  /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+  const sections = document.querySelectorAll(
+    "section[id]"
+  ) as NodeListOf<HTMLElement>;
+
+  const scrollActive = () => {
+    const scrollDown = window.scrollY;
+
+    sections.forEach((current: HTMLElement) => {
+      const sectionHeight = current.offsetHeight,
+        sectionTop = current.offsetTop - 58,
+        sectionId = current.getAttribute("id"),
+        sectionsClass = document.querySelector(
+          ".nav__menu a[href*=" + sectionId + "]"
+        );
+
+      if (!sectionsClass) return;
+
+      if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+        sectionsClass.classList.add("active-link");
+      } else {
+        sectionsClass.classList.remove("active-link");
+      }
+    });
+  };
+  window.addEventListener("scroll", scrollActive);
+});
+</script>
+
+<template>
+  <a href="#" class="scrollup" id="scroll-up">
+    <i class="ri-arrow-up-line"></i>
+  </a>
+</template>
