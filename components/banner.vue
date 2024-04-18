e42f5cfb85c3ade3470b40e6e0e10faeef2f681a
isiah <jones.isiah.blhs@gmail.com>
2024-04-17 20:55:13 -0400

    create banner component

---
+++ components/banner.vue
@@ -0,0 +1,21 @@
+<script lang="ts" setup></script>
+
+<template>
+  <section class="banner">
+    <div class="banner__container">
+      <ul class="banner__list">
+        <li class="banner__item">
+          <i class="ri-leaf-line"></i>
+          <span>GOOD FOR NATURE</span>
+        </li>
+
+        <li class="banner__item">
+          <i class="ri-heart-pulse-line"></i>
+          <span>LOW CALORIES</span>
+        </li>
+      </ul>
+    </div>
+  </section>
+</template>
+
+<style></style>
